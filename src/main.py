3b425405105ab2be7b6582ccecd0fc8374a6c84b
Elouan Vincent <elouan.vincent@epita.fr>
2023-10-23 15:45:12 +0200

    add: word2vec and utils function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from dataset import LargeMovieDataset
-
-
+from gensim.models import Word2Vec
+from utils import get_sentances_data, check_dir
 
 
 if __name__ == '__main__':
+    MODELS_DIR = 'models'
+    check_dir(MODELS_DIR)
     # Create dataset
-    dataset = LargeMovieDataset(path='../data/aclImdb_v1/aclImdb')
+    sentances = get_sentances_data(path='data/aclImdb_v1/aclImdb/train')
+    word_embedding_size = 64
+    word2vec_model = Word2Vec(sentances, vector_size=word_embedding_size, window=5, min_count=1, workers=4)
+    word2vec_model.save(f'{MODELS_DIR}/word2vec_model.model')
+    word2vec_model = Word2Vec.load(f'{MODELS_DIR}/word2vec_model.model')
+
+    dataset = LargeMovieDataset(path='data/aclImdb_v1/aclImdb', set='train', embedding_dic=word2vec_model.wv)
     print('Dataset size:', len(dataset))
     print('First sample:', dataset[0])
     print('Second sample:', dataset[1])
