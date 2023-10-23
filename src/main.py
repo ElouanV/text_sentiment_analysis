8b405b6093146bace36dd2fb0e7eb810eab9cca0
erwann <erwann.mevel@epita.fr>
2023-10-23 17:19:30 +0200

    fix merge

--- src/main.py
+++ src/main.py
@@ -7,14 +7,8 @@ if __name__ == '__main__':
     MODELS_DIR = 'models'
     check_dir(MODELS_DIR)
     # Create dataset
-    sentences = get_sentences_data(path='data/aclImdb_v1/aclImdb/train')
     word_embedding_size = 16
-    word2vec_model = Word2Vec(sentences, vector_size=word_embedding_size, window=3, min_count=1, workers=4)
-    word2vec_model.save(f'{MODELS_DIR}/word2vec_model.model')
     word2vec_model = Word2Vec.load(f'{MODELS_DIR}/word2vec_model.model')
 
-    train_set = LargeMovieDataset(path='data/aclImdb_v1/aclImdb', set='train', embedding_dic=word2vec_model.wv, word_embedding_size=word_embedding_size)
-    test_set = LargeMovieDataset(path='data/aclImdb_v1/aclImdb', set='test', embedding_dic=word2vec_model.wv, word_embedding_size=word_embedding_size)
-
-    # Create dataloader
-
+    train_set = LargeMovieDataset(path='../aclImdb_v1/', set='train', embedding_dic=word2vec_model.wv, word_embedding_size=word_embedding_size)
+    test_set = LargeMovieDataset(path='../aclImdb_v1/', set='test', embedding_dic=word2vec_model.wv, word_embedding_size=word_embedding_size)
